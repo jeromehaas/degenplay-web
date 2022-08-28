c6b519ce64954f2fc4f72873844e4506febda9e3
jeromehaas <hello@jeromehaas.ch>
2022-08-28 22:48:49 +0200

    Setup: responsiveness of faq component

--- src/components/02-molecules/accordion/accordion.jsx
+++ src/components/02-molecules/accordion/accordion.jsx
@@ -1,22 +1,35 @@
 import Heading from "components/01-atoms/heading/heading";
 import Text from "components/01-atoms/text/text";
+import { useRef } from 'react';
 
 const Accordion = ({ items}) => {
  
+    const reference = useRef();
+
     const handleClick = (event) => {
         const clickedItem = event.target.closest('.accordion__item');
+        const clickedItemText = clickedItem.querySelector('.item__text');
+        const clickedItemIcon = clickedItem.querySelector('.item__icon');
+        const clickedItemHeight = clickedItem.scrollHeight + clickedItemText.scrollHeight;
         const allItems = document.querySelectorAll('.accordion__item');
         allItems.forEach((item) => {
-            if (item === clickedItem) return;
-            item.classList.remove('item--open')
+            const text = item.querySelector('.item__text');
+            const icon = item.querySelector('.item__icon'); 
+            text.style.maxHeight = '0px';
+            text.style.opacity = '0';
+            text.style.paddingTop = '0px';
+            icon.style.transform = 'rotate(0deg)';
         });
-        clickedItem.classList.toggle('item--open');
+        clickedItemText.style.maxHeight = `${clickedItemHeight}px`;
+        clickedItemText.style.opacity = '1';
+        clickedItemText.style.paddingTop = '16px';
+        clickedItemIcon.style.transform = 'rotate(180deg)';
     };
 
     return (
         <div className="accordion">
             { items && items.map((item, index) => (
-                <div className="accordion__item item" key={ index } onClick={ handleClick }>
+                <div className="accordion__item item" key={ index } onClick={ handleClick } ref={ reference } >
                     <Text className="item__heading">{ item.question }</Text>
                     <i className="item__icon icon icon--caret"></i>
                     <Text className="item__text">{ item.answer }</Text>
